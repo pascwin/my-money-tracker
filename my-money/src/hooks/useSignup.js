f29fd11c536f8f2f0deb46d204baa3d6ceba74c7
“Pascwin” <pascal-winner@web.de>
2022-10-15 13:40:04 +0200

    did some firebase changes

--- my-money/src/hooks/useSignup.js
+++ my-money/src/hooks/useSignup.js
@@ -1,55 +1,46 @@
-import { useState, useEffect } from "react"
-import { projectAuth } from "../firebase/config"
-import { useAuthContext } from "./useAuthContext"
+import { useState, useEffect } from 'react'
+import { projectAuth } from '../firebase/config'
+import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
-    const [isCancelled, setIsCancelled] = useState(false)
-    const [error, setError] = useState(null)
-    const [isPending, setIsPending] = useState(false)
-    const { dispatch } = useAuthContext()
+  const [isCancelled, setIsCancelled] = useState(false)
+  const [error, setError] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const { dispatch } = useAuthContext()
 
-    const signup = async (email, password, displayName) => {
+  const signup = async (email, password, displayName) => {
+    setError(null)
+    setIsPending(true)
+  
+    try {
+      // signup
+      const res = await projectAuth.createUserWithEmailAndPassword(email, password)
+
+      if (!res) {
+        throw new Error('Could not complete signup')
+      }
+
+      // add display name to user
+      await res.user.updateProfile({ displayName })
+      // dispatch login action
+      dispatch({ type: 'LOGIN', payload: res.user })
+
+      if (!isCancelled) {
+        setIsPending(false)
         setError(null)
-        setIsPending(true)
-
-        try {
-            //signup user
-            const res = await projectAuth.createUserWithEmailAndPassword(email, password)
-
-            if (!res.ok) {
-                throw new Error("could not complete signup")
-            }
-            //add display name to user
-            await res.user.updateProfile({ displayName })
-
-            //dispatch login action
-            dispatch({ type: "LOGIN", payload: res.user })
-
-            //update state
-            if (!isCancelled) {
-                setIsPending(false)
-                setError(null)
-            }
-
-        } catch (err) {
-            if (!isCancelled) {
-                console.log(err)
-                console.log(err.message)
-                setError(err.message);
-                setIsPending(false);
-            }
-        }
+      }
+    } 
+    catch(err) {
+      if (!isCancelled) {
+        setError(err.message)
+        setIsPending(false)
+      }
     }
+  }
 
-    useEffect(() => {
-        return () => {
-            setIsCancelled(true)
-        }
-    }, [])
+  useEffect(() => {
+    return () => setIsCancelled(true)
+  }, [])
 
-    return {
-        error,
-        isPending,
-        signup
-    }
+  return { signup, error, isPending }
 }
--- my-money/src/pages/home/TransactionList.js
+++ my-money/src/pages/home/TransactionList.js
@@ -17,7 +17,6 @@ const TransactionList = ({ transactions }) => {
                     )
                 })
             }
-
         </ul>
     )
 }
